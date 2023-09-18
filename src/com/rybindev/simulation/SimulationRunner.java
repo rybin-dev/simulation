8dc9ce849ea3d7fe9f541f4c191eb880b8a4dca7
rybin-dev <rybin-dev@ya.ru>
2023-09-18 15:13:08 +0300

    simulation

--- src/com/rybindev/simulation/SimulationRunner.java
+++ src/com/rybindev/simulation/SimulationRunner.java
@@ -21,6 +21,10 @@ import com.rybindev.simulation.system.PredatorMovementSystem;
 import com.rybindev.simulation.system.PredatorSpawnSystem;
 import com.rybindev.simulation.system.PredatorTargetSearchSystem;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -29,70 +33,140 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Random;
 
-public class SimulationRunner {
+public class SimulationRunner extends JPanel implements ActionListener {
 
-    public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
+
+    private static HerbivoreTargetSearchSystem herbivoreTargetSearchSystem;
+    private static PredatorTargetSearchSystem predatorTargetSearchSystem;
+    private static FindPathSystem findPathSystem;
+    private static HerbivoreMovementSystem herbivoreMovementSystem;
+    private static PredatorMovementSystem predatorMovementSystem;
+    private static DamageSystem damageSystem;
+    private static CrassSpawnSystem crassSpawnSystem;
+    private static HerbivoreSpawnSystem herbivoreSpawnSystem;
+    private static PredatorSpawnSystem predatorSpawnSystem;
+
+    private final int DELAY = 300;
+
+    private Timer timer;
+    private Image crass;
+    private Image herbivore;
+    private Image predator;
+    private GraphicsSystem graphicsSystem;
+    private Coordinator coordinator;
+
+    public SimulationRunner() {
+        init();
+    }
+
+    void init() {
         Random random = new Random();
 
         Graphics graphics = new Graphics(Config.WIDTH, Config.HEIGHT);
 
-        Coordinator coordinator = new Coordinator(Config.MAX_ENTITIES);
+        coordinator = new Coordinator(Config.MAX_ENTITIES);
 
 
-        for (Class<?> clazz : getClasses("com.rybindev.simulation.component")) {
-            coordinator.registerComponent(clazz);
+        try {
+            for (Class<?> clazz : getClasses("com.rybindev.simulation.component")) {
+                coordinator.registerComponent(clazz);
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
         }
 
-        GraphicsSystem graphicsSystem = registerSystem(coordinator, GraphicsSystem.class, SpriteComponent.class, PositionComponent.class);
+        graphicsSystem = registerSystem(coordinator, GraphicsSystem.class, SpriteComponent.class, PositionComponent.class);
         graphicsSystem.setCoordinator(coordinator);
         graphicsSystem.setGraphics(graphics);
 
-        HerbivoreTargetSearchSystem herbivoreTargetSearchSystem = registerSystem(coordinator, HerbivoreTargetSearchSystem.class, CrassMarker.class);
+        herbivoreTargetSearchSystem = registerSystem(coordinator, HerbivoreTargetSearchSystem.class, CrassMarker.class);
         herbivoreTargetSearchSystem.setCoordinator(coordinator);
 
-        PredatorTargetSearchSystem predatorTargetSearchSystem = registerSystem(coordinator, PredatorTargetSearchSystem.class, HerbivoreMarker.class);
+        predatorTargetSearchSystem = registerSystem(coordinator, PredatorTargetSearchSystem.class, HerbivoreMarker.class);
         predatorTargetSearchSystem.setCoordinator(coordinator);
 
-        FindPathSystem findPathSystem = registerSystem(coordinator, FindPathSystem.class, PositionComponent.class);
+        findPathSystem = registerSystem(coordinator, FindPathSystem.class, PositionComponent.class);
         findPathSystem.setCoordinator(coordinator);
 
-        HerbivoreMovementSystem herbivoreMovementSystem = registerSystem(coordinator, HerbivoreMovementSystem.class, HerbivoreMarker.class);
+        herbivoreMovementSystem = registerSystem(coordinator, HerbivoreMovementSystem.class, HerbivoreMarker.class);
         herbivoreMovementSystem.setCoordinator(coordinator);
         herbivoreMovementSystem.setTargetSearchSystem(herbivoreTargetSearchSystem);
         herbivoreMovementSystem.setFindPathSystem(findPathSystem);
 
-        PredatorMovementSystem predatorMovementSystem = registerSystem(coordinator, PredatorMovementSystem.class, PredatorMarker.class);
+        predatorMovementSystem = registerSystem(coordinator, PredatorMovementSystem.class, PredatorMarker.class);
         predatorMovementSystem.setCoordinator(coordinator);
         predatorMovementSystem.setTargetSearchSystem(predatorTargetSearchSystem);
         predatorMovementSystem.setFindPathSystem(findPathSystem);
 
-        DamageSystem damageSystem = registerSystem(coordinator, DamageSystem.class, DamageRequestComponent.class, HealthComponent.class);
+        damageSystem = registerSystem(coordinator, DamageSystem.class, DamageRequestComponent.class, HealthComponent.class);
         damageSystem.setCoordinator(coordinator);
 
-        CrassSpawnSystem crassSpawnSystem = registerSystem(coordinator, CrassSpawnSystem.class, CrassMarker.class);
+        crassSpawnSystem = registerSystem(coordinator, CrassSpawnSystem.class, CrassMarker.class);
         crassSpawnSystem.setCoordinator(coordinator);
         crassSpawnSystem.setRandom(random);
 
-        HerbivoreSpawnSystem herbivoreSpawnSystem = registerSystem(coordinator, HerbivoreSpawnSystem.class, HerbivoreMarker.class);
+        herbivoreSpawnSystem = registerSystem(coordinator, HerbivoreSpawnSystem.class, HerbivoreMarker.class);
         herbivoreSpawnSystem.setCoordinator(coordinator);
         herbivoreSpawnSystem.setRandom(random);
 
-        PredatorSpawnSystem predatorSpawnSystem = registerSystem(coordinator, PredatorSpawnSystem.class, PredatorMarker.class);
+        predatorSpawnSystem = registerSystem(coordinator, PredatorSpawnSystem.class, PredatorMarker.class);
         predatorSpawnSystem.setCoordinator(coordinator);
         predatorSpawnSystem.setRandom(random);
 
+        setBackground(Color.black);
+        setFocusable(true);
 
-        for (int i = 0; i < 2000; i++) {
-            crassSpawnSystem.update();
-            herbivoreSpawnSystem.update();
-            predatorSpawnSystem.update();
-            graphicsSystem.update();
-            herbivoreMovementSystem.update();
-            predatorMovementSystem.update();
-            damageSystem.update();
-            Thread.sleep(10);
+        setPreferredSize(new Dimension(Config.WIDTH*10, Config.HEIGHT*10));
+        loadImages();
 
-        }
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    private void loadImages() {
+
+        ImageIcon iid = new ImageIcon("src/resources/dot.png");
+        crass = iid.getImage();
+
+        ImageIcon iia = new ImageIcon("src/resources/apple.png");
+        herbivore = iia.getImage();
+
+        ImageIcon iih = new ImageIcon("src/resources/head.png");
+        predator = iih.getImage();
+    }
+
+
+    @Override
+    public void paintComponent(java.awt.Graphics g) {
+        super.paintComponent(g);
+        graphicsSystem.getEntities().forEach(entity -> {
+            PositionComponent position = coordinator.getComponent(entity, PositionComponent.class);
+            SpriteComponent sprite = coordinator.getComponent(entity, SpriteComponent.class);
+
+            switch (sprite.getSprite()){
+                case "v" -> g.drawImage(crass,position.getX()*10,position.getY()*10,this);
+                case "@"-> g.drawImage(herbivore,position.getX()*10,position.getY()*10,this);
+                case "#"-> g.drawImage(predator,position.getX()*10,position.getY()*10,this);
+            }
+            Toolkit.getDefaultToolkit().sync();
+        });
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new JFrame();
+            ex.add(new SimulationRunner());
+
+            ex.setResizable(false);
+            ex.pack();
+
+            ex.setTitle("Simulation");
+            ex.setLocationRelativeTo(null);
+            ex.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            ex.setVisible(true);
+        });
+
 
     }
 
@@ -154,4 +228,16 @@ public class SimulationRunner {
         return classes;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        crassSpawnSystem.update();
+        herbivoreSpawnSystem.update();
+        predatorSpawnSystem.update();
+        herbivoreMovementSystem.update();
+        predatorMovementSystem.update();
+        damageSystem.update();
+
+        repaint();
+
+    }
 }
